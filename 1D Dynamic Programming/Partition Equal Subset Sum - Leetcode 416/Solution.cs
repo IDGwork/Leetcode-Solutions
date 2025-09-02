fd8b05253169121976c394f0e2401ca093837b06
CP Training <cptraining@Ignass-MacBook-Pro.local>
2025-09-02 17:03:30 +0100

    Update

--- 1D Dynamic Programming/Partition Equal Subset Sum - Leetcode 416/Solution.cs
+++ 1D Dynamic Programming/Partition Equal Subset Sum - Leetcode 416/Solution.cs
@@ -2,5 +2,24 @@ namespace Leetcode_Solutions._1D_Dynamic_Programming.Partition_Equal_Subset_Sum_
 
 public class Solution416
 {
-    
+    public bool CanPartition(int[] nums)
+    {
+        var total = 0;
+        foreach (var num in nums) total += num;
+        if (total % 2 != 0) return false;
+
+        var target = total / 2;
+        var dp = new bool[target + 1];
+        dp[0] = true;
+
+        foreach (var num in nums)
+        {
+            for (var i = target; i >= num; i--)
+            {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
+    } 
 }
