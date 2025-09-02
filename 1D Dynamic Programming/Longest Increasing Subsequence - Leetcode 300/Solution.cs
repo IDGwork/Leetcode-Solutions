485e41da2dcde36c377efbc2fddd71c613118f4f
CP Training <cptraining@Ignass-MacBook-Pro.local>
2025-09-02 16:53:24 +0100

    Update

--- 1D Dynamic Programming/Longest Increasing Subsequence - Leetcode 300/Solution.cs
+++ 1D Dynamic Programming/Longest Increasing Subsequence - Leetcode 300/Solution.cs
@@ -2,5 +2,29 @@ namespace Leetcode_Solutions._1D_Dynamic_Programming.Longest_Increasing_Subseque
 
 public class Solution300
 {
-    
+    public int LengthOfLIS(int[] nums)
+    {
+        var res = 0;
+        
+        var dp = new int[nums.Length];
+        Array.Fill(dp, 1);
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            res = Math.Max(res, dp[i]);
+        }
+
+        return res;
+    }
 }
