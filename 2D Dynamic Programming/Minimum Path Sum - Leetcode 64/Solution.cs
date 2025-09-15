b5de9353499102104bd36a2f6735629bd967c0f0
CP Training <cptraining@Ignass-MacBook-Pro.local>
2025-09-15 10:27:12 +0100

    Update

---
+++ 2D Dynamic Programming/Minimum Path Sum - Leetcode 64/Solution.cs
@@ -0,0 +1,25 @@
+namespace Leetcode_Solutions._2D_Dynamic_Programming.Minimum_Path_Sum___Leetcode_64;
+
+public class Solution64
+{
+    public int MinPathSum(int[][] grid)
+    {
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+
+        var dp = new int[rows, cols];
+        dp[0, 0] = grid[0][0];
+        for (var r = 1; r < rows; r++) dp[r, 0] = dp[r - 1, 0] + grid[r][0];
+        for (var c = 1; c < cols; c++) dp[0, c] = dp[0, c - 1] + grid[0][c];
+
+        for (var r = 1; r < rows; r++)
+        {
+            for (var c = 1; c < cols; c++)
+            {
+                dp[r, c] = Math.Min(dp[r - 1, c], dp[r, c - 1]) + grid[r][c];
+            }
+        }
+
+        return dp[rows - 1, cols - 1];
+    }
+}
